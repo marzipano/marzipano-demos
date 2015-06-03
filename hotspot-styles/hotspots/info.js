c0e0bee1248376d9cfe8f9231a767870143902ac
Manuel Cabral <m.cabral@digisfera.com>
2015-06-03 09:13:55 +0200

    Fix hotspot-styles z-index

--- hotspot-styles/hotspots/info.js
+++ hotspot-styles/hotspots/info.js
@@ -3,19 +3,6 @@ scene.hotspotContainer().createHotspot(document.querySelector("#info"), { yaw: -
 document.querySelector("#info .icon_wrapper").addEventListener('click', function() {
   document.querySelector("#info").classList.toggle('expanded');
   document.querySelector("#inner_icon").classList.toggle('closeIcon');
-
-  if(document.querySelector("#info").classList.contains('expanded')) {
-    var position = viewer.view().coordinatesToScreen({ yaw: -0.1, pitch: 0.1 });
-
-    // Center the modal horizontally and adjust it vertically
-    // It is not possible to precisely center vertically as we do not know its size
-    position.x += 175;
-    position.y += 150;
-
-    var coords = viewer.view().screenToCoordinates(position, coords);
-
-    viewer.lookTo(coords, { transitionDuration: 300 });
-  }
 });
 
 document.querySelector("#info .close").addEventListener('click', function() {
